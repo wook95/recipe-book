7d47232a1d9ff0122382f75f50e1506ebefabf2a
이욱창 <uclee95@gmail.com>
2022-04-23 01:30:22 +0900

    style: 헤더 스타일 구현

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,5 +5,10 @@ import {Component} from "@angular/core";
   templateUrl:'./header.component.html'
 })
 export class  HeaderComponent{
+  collapsed:boolean
+
+  constructor() {
+    this.collapsed=true
+  }
 
 }
