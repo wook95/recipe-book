1db9873e6630d2df800108cd982aff7ecebeec73
이욱창 <uclee95@gmail.com>
2022-04-23 14:47:37 +0900

    ui: 레시피리스트 컴포넌트 마크업 구현

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Input() recipes: Recipe[]
 
   constructor() { }
 
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from './recipes.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('a test recipe','this is simply a test','https://learnenglishteens.britishcouncil.org/sites/teens/files/a_recipe_1.jpg')
+  ];
 
   constructor() { }
 
