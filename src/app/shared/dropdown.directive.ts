292b2056fc995bfd3ae0ca7b2c6af09e069a1f66
이욱창 <uclee95@gmail.com>
2022-04-24 21:05:02 +0900

    feat: 디렉티브를 통한 드롭다운 구현

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,22 @@
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
+
+@Directive({
+  selector:'[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click',['$event']) toggleOpen(event:Event) {
+      this.isOpen = this.elementRef.nativeElement.contains(event.target)? !this.isOpen:false;
+    }
+
+  constructor(private  elementRef:ElementRef,private renderer:Renderer2) {
+  }
+
+
+}
