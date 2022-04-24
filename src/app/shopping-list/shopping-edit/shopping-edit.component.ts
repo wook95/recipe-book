df9f97d148eec0f2b4766486d741aff857d86cc5
이욱창 <uclee95@gmail.com>
2022-04-24 19:24:58 +0900

    feat: 로컬 참조를 통한 쇼핑리스트 삽입 구현

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent{
+  @ViewChild('nameInput') nameInput:ElementRef;
+  @ViewChild('amountInput') amountInput:ElementRef;
+  @Output() ingredientsState = new EventEmitter<Ingredient>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  onSubmit(event) {
+    event.preventDefault();
+    const ingredientName = this.nameInput.nativeElement.value;
+    const ingredientAmount = this.amountInput.nativeElement.value;
+    if (ingredientName && ingredientAmount) {
+      this.ingredientsState.emit(new Ingredient(ingredientName,ingredientAmount));
+    }else {
+      window.alert('빈칸을 다 채워주세요.')
+    }
+
   }
 
 }
